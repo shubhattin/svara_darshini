a147112663c6930cdf3061971dae4bae25dab99e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-12-24 22:35:40 +0530

    init

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,10 +1,92 @@
 <script lang="ts">
+  import {
+    Chart,
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+    CategoryScale
+  } from 'chart.js';
+  import { onMount } from 'svelte';
+  import { scale, slide } from 'svelte/transition';
+
   let frequency = $state(0);
   let is_listening = $state(false);
+  let chart: Chart | null = null;
+  let freequency_data = $state<number[]>(Array.from({ length: 100 }).map(() => 0));
+
+  onMount(() => {
+    Chart.register(
+      LineController,
+      LineElement,
+      PointElement,
+      LinearScale,
+      Title,
+      Tooltip,
+      Legend,
+      CategoryScale
+    );
+  });
 
   let graph_canvas: HTMLCanvasElement = $state(null!);
+
   $effect(() => {
-    if (!is_listening) return;
+    if (!is_listening) {
+      freequency_data = [];
+      return;
+    }
+    // const data = freequency_data.slice(-100);
+    // chart!.data.datasets[0].data = data;
+  });
+
+  $effect(() => {
+    if (!is_listening || !graph_canvas) return;
+    if (chart) chart.destroy();
+    chart = new Chart(graph_canvas, {
+      type: 'line',
+      data: {
+        labels: freequency_data.map((v, i) => `${i + 1}`),
+        datasets: [
+          {
+            label: 'Frequency',
+            data: freequency_data,
+            borderColor: '#007bff',
+            backgroundColor: '#007bff',
+            fill: false,
+            tension: 0.4,
+            pointRadius: 0,
+            pointHoverRadius: 0,
+            pointHitRadius: 0
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: 'Frequency'
+          },
+          tooltip: {
+            enabled: false
+          }
+        },
+        scales: {
+          x: {
+            display: false
+          },
+          y: {
+            display: true
+          }
+        }
+      }
+    });
   });
 
   async function startFrequencyDetection() {
@@ -47,7 +129,7 @@
 </script>
 
 <svelte:head>
-  <title>Freequency Reader</title>
+  <title>Frequency Reader</title>
 </svelte:head>
 
 <div class="mt-4 space-x-6">
@@ -68,9 +150,13 @@
 </div>
 
 {#if is_listening}
-  <div class="mt-4 text-xl font-bold">
-    Detected Frequency: <strong>{frequency} Hz</strong>
-    <div class="mt-4 rounded-lg border-2 border-gray-700">
+  <div in:scale out:slide class="mt-4 text-xl font-bold">
+    <div class="font-semibold">
+      Detected Frequency: <span class="font-bold text-primary-500 dark:text-primary-400"
+        >{frequency} Hz</span
+      >
+    </div>
+    <div class="mt-4 rounded-lg border-2 border-gray-700 p-1.5">
       <canvas bind:this={graph_canvas}></canvas>
     </div>
   </div>
