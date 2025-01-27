735ab8338d3b687a3c64f9c899cea382fd12701f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-01-27 23:04:56 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss()],
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SvaraDarshini',
+        short_name: 'SvaraDarshini',
+        start_url: '/',
+        scope: './',
+        icons: [
+          {
+            src: 'img/icon_128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        theme_color: '#181e20',
+        background_color: '#333',
+        display: 'standalone'
+      }
+    })
+  ],
   worker: {
     format: 'es'
   }
