72c9fc6edfbfb4727cc548d05c94a5028ed61648
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-01-29 11:50:53 +0530

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-import { generateRobotsTxt } from './src/tools/plugins/robots_txt';
+import { generateRobotsTxtSitemap } from './src/tools/plugins/robots_txt_sitemap';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss(), generateRobotsTxt()],
+  plugins: [sveltekit(), purgeCss(), generateRobotsTxtSitemap()],
   worker: {
     format: 'es'
   }
