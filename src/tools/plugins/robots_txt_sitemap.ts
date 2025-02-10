e2e01be8a056af24b3d71f1a96b061f423875919
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-10 16:22:40 +0530

    robots

--- src/tools/plugins/robots_txt_sitemap.ts
+++ src/tools/plugins/robots_txt_sitemap.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
 import type { PluginOption } from 'vite';
+import * as fs from 'fs';
 // @ts-ignore
 import robotstxt from 'generate-robotstxt';
 
@@ -13,7 +14,7 @@ const make_robots_txt = async () => {
     policy: [
       {
         userAgent: '*',
-        disallow: ['/_app/', '/~partytown/', '/img/', '/manifest.json'],
+        disallow: ['/_app/', '/img/', '/manifest.json'],
         crawlDelay: 2
       }
     ],
@@ -23,7 +24,10 @@ const make_robots_txt = async () => {
 };
 
 const make_sitemap = async () => {
-  const links = [{ url: '/', changefreq: 'weekly', priority: 1 }];
+  const links = [
+    { url: '/', changefreq: 'weekly', priority: 1 },
+    { url: '/convert', changefreq: 'weekly', priority: 0.8 }
+  ];
   const stream = new SitemapStream({
     hostname: BASE_URL
   });
@@ -38,6 +42,18 @@ export function generateRobotsTxtSitemap() {
     name: 'generate-robots-txt-sitemap-xml',
     async buildStart() {
       await Promise.all([make_robots_txt(), make_sitemap()]);
+    },
+    async closeBundle() {
+      // Update the manifest to exclude robots.txt and sitemap.xml from edge functions
+      const manifest_path = '.netlify/edge-functions/manifest.json';
+      if (fs.existsSync(manifest_path)) {
+        const manifest = JSON.parse(fs.readFileSync(manifest_path, 'utf-8'));
+        for (let to_add of ['robots.txt', 'sitemap.xml']) {
+          if (!manifest.functions[0].excludedPath.includes(`/${to_add}`))
+            manifest.functions[0].excludedPath.push(`/${to_add}`);
+        }
+        writeFileSync(manifest_path, JSON.stringify(manifest));
+      }
     }
   } as PluginOption;
 }
