24975db4e8ae51352d078d2ceacb67a83f5dd48f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-23 18:55:15 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,45 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { generateRobotsTxtSitemap } from './src/tools/plugins/robots_txt_sitemap';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss(), generateRobotsTxtSitemap()],
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    generateRobotsTxtSitemap(),
+    SvelteKitPWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'SvaraDarshini',
+        short_name: 'SvaraDarshini',
+        start_url: '/',
+        scope: './',
+        icons: [
+          {
+            src: 'img/icon_128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        theme_color: '#181e20',
+        background_color: '#333',
+        display: 'standalone'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg}']
+      }
+    })
+  ],
   worker: {
     format: 'es'
   }
