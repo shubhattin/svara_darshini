403babbe23c7a0b583068ad9e341456a0d3b1580
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-24 17:05:20 +0530

    sw fix

--- src/components/tags/ServiceWorker.svelte
+++ src/components/tags/ServiceWorker.svelte
@@ -6,6 +6,16 @@
   onMount(() => {
     if (browser && import.meta.env.PROD) {
       registerSW({ immediate: true });
+      if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker
+            .register('/sw.js')
+            .then((registration) => {})
+            .catch((error) => {
+              console.log('SW registration failed:', error);
+            });
+        });
+      }
     }
   });
 </script>
