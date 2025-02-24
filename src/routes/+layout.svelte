4a9002e6560286a7bbd2679abc5790c396463387
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-24 14:25:26 +0530

    Add Service Worker for Offline Support (#5)
    * test
    
    * man
    
    * asd
    
    * qw
    
    * app
    
    * test without party
    
    * restore
    
    * vite
    
    * asd
    
    * asd
    
    * test
    
    * asd
    
    * kigari
    
    * dine
    
    * final kind
    
    * service
    
    * update
    
    * upadte test fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,3 +26,20 @@
   </div>
 </div>
 <PostHogInit />
+
+<svelte:head>
+  {#if import.meta.env.PROD}
+    <script>
+      if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker
+            .register('/sw.js')
+            .then((registration) => {})
+            .catch((error) => {
+              console.log('SW registration failed:', error);
+            });
+        });
+      }
+    </script>
+  {/if}
+</svelte:head>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,51 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { generateRobotsTxtSitemap } from './src/tools/plugins/robots_txt_sitemap';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-  plugins: [sveltekit(), purgeCss(), generateRobotsTxtSitemap()],
-  worker: {
-    format: 'es'
-  }
+  plugins: [
+    sveltekit(),
+    purgeCss(),
+    generateRobotsTxtSitemap(),
+    SvelteKitPWA({
+      strategies: 'generateSW',
+      registerType: 'autoUpdate',
+      injectRegister: 'script',
+      // ^ The option does not seem top work so we are adding it manually in +layout.svelte
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg,ttf,woff2,otf}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      manifest: {
+        name: 'SvaraDarshini',
+        short_name: 'SvaraDarshini',
+        description:
+          'Svara Darshini is an intuitive tool to understand Principles of Music that are common across the world',
+        theme_color: '#181e20',
+        background_color: '#333',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: 'img/icon_128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 });
