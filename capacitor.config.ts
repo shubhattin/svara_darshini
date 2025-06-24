3c7af2a80c3b4cd809da4ce6c0915c8c6a9755c4
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-06-24 17:26:50 +0530

    build apk 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.svara_darshini_tsc.app',
   appName: 'Svara Darshini',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    StatusBar: {
+      style: 'Dark',
+      backgroundColor: '#1e293b',
+      overlaysWebView: false
+    }
+  }
 };
 
 export default config;
