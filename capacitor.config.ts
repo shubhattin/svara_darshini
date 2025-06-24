381d97055ef5b457ca2eaa203588400056fd5736
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-06-24 11:19:59 +0530

    cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.svara_darshini_tsc.app',
+  appName: 'Svara Darshini',
+  webDir: 'build'
+};
+
+export default config;
