ccfd06e28f346ffe70e27aac93a7c4dc3b4aa808
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2026-01-21 15:05:00 +0530

    posthog

--- src/components/tags/PostHogInit.svelte
+++ src/components/tags/PostHogInit.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { browser } from '$app/environment';
+  import { Capacitor } from '@capacitor/core';
+
+  const is_native_platform = Capacitor.isNativePlatform();
 
   onMount(() => {
     if (
-      browser &&
       import.meta.env.PROD &&
       import.meta.env.VITE_POSTHOG_KEY &&
       (import.meta.env.VITE_POSTHOG_URL || import.meta.env.VITE_SITE_URL)
     ) {
-      import('posthog-js').then((posthog) => {
-        posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
-          api_host: `${import.meta.env.VITE_POSTHOG_URL}`,
-          person_profiles: 'identified_only',
-          ui_host: 'https://us.posthog.com'
+      if (browser && !is_native_platform) {
+        import('posthog-js').then((posthog) => {
+          posthog.default.init(import.meta.env.VITE_POSTHOG_KEY, {
+            api_host: `${import.meta.env.VITE_POSTHOG_URL}`,
+            person_profiles: 'identified_only',
+            ui_host: 'https://us.posthog.com'
+          });
         });
-      });
+      }
+      if (is_native_platform) {
+        import('@capawesome/capacitor-posthog').then((posthog) => {
+          posthog.Posthog.setup({
+            apiKey: import.meta.env.VITE_POSTHOG_KEY,
+            host: import.meta.env.VITE_POSTHOG_URL
+          });
+        });
+      }
     }
   });
 </script>
